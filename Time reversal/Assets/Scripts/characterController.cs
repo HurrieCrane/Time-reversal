39bf95669b92b45b0efe9bcbc0d5925ad69ed35f
Craneio <joec@vandie.co.uk>
2017-07-16 19:41:30 +0100

    Adding .gitignore as well as the Unity project itself

---
+++ Time reversal/Assets/Scripts/characterController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody R;
+
+	void Start () {
+        R = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+
+        float z = 0;
+        float y = 0;
+        float x = 0;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            z += speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            z -= speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            x -= speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            x += speed;
+        }
+
+        transform.position += new Vector3(x, y, z);
+    }
+}
---
+++ Time reversal/Assets/Scripts/tracker.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tracker : MonoBehaviour {
+
+    public float frequency;
+    public bool record = true;
+
+    public Stack<Vector3> pointsOverTime = new Stack<Vector3>();
+
+    private void Start()
+    {
+        StartCoroutine("recordPer");
+    }
+
+    private void Update()
+    {
+        Vector3[] points = pointsOverTime.ToArray();
+
+        for(int i = 0; i < points.Length; i++)
+        {
+            if(i != 0 && i != points.Length)
+            {
+                Debug.DrawLine(points[i-1], points[i]);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Return))
+        {
+            record = false;
+            StopAllCoroutines();
+
+            StartCoroutine("reverse");
+        }
+    }
+
+    IEnumerator reverse()
+    {
+        GetComponent<characterController>().enabled = false;
+
+        foreach(Vector3 v in pointsOverTime)
+        {
+            transform.position = v;
+            yield return null;
+        }
+
+        pointsOverTime = new Stack<Vector3>();
+
+        record = true;
+        GetComponent<characterController>().enabled = true;
+
+        StartCoroutine("recordPer");
+    }
+
+    IEnumerator recordPer()
+    {
+        while(record == true)
+        {
+            pointsOverTime.Push(transform.position);
+            yield return new WaitForSeconds(frequency);
+        }
+    }
+}
